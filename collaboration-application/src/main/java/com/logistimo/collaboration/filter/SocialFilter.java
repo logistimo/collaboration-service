afe8fb1f0a2d0dbea356161744c6cc20622bc013
kumargaurav <kumar.gaurav@logistimo.com>
2017-12-20 15:32:26 +0530

    refactored code to address sonar issues

--- collaboration-application/src/main/java/com/logistimo/collaboration/filter/SocialFilter.java
+++ collaboration-application/src/main/java/com/logistimo/collaboration/filter/SocialFilter.java
@@ -2,12 +2,11 @@ package com.logistimo.collaboration.filter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
 import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,24 +15,14 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * Created by kumargaurav on 09/11/17.
  */
-public class SocialFilter implements Filter {
+public class SocialFilter extends GenericFilterBean {
 
-  private static final Logger logger = LoggerFactory.getLogger(SocialFilter.class);
-
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-
-  }
+  private static final Logger log = LoggerFactory.getLogger(SocialFilter.class);
 
   @Override
   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
                        FilterChain filterChain) throws IOException, ServletException {
 
-    logger.info("Request for resource {}",((HttpServletRequest)servletRequest).getRequestURI());
-  }
-
-  @Override
-  public void destroy() {
-
+    log.info("Request for resource {}",((HttpServletRequest)servletRequest).getRequestURI());
   }
 }
