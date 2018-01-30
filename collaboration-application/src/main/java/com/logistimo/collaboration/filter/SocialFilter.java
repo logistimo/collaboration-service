d8901857b623152231b2679af30a69e951d59a27
kumargaurav <kumar.gaurav@logistimo.com>
2018-01-30 17:22:20 +0530

    added missing line

--- collaboration-application/src/main/java/com/logistimo/collaboration/filter/SocialFilter.java
+++ collaboration-application/src/main/java/com/logistimo/collaboration/filter/SocialFilter.java
@@ -24,5 +24,6 @@ public class SocialFilter extends GenericFilterBean {
                        FilterChain filterChain) throws IOException, ServletException {
 
     log.info("Request for resource {}",((HttpServletRequest)servletRequest).getRequestURI());
+    filterChain.doFilter(servletRequest,servletResponse);
   }
 }
